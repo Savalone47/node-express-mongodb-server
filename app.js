f003832afef67b53c0aecb2115b87022c1a36c2f
savalone47 <savalonescott47@gmail.com>
2020-03-13 22:24:39 +0200

    consign

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ import express from "express";
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.set("json space", 4);
+
 app.get("/",(req, res)=>{
   res.json({status: "Task API"});
 });
 
+app.get("/node",(req, res)=>{
+  res.json({
+    Tasks: [
+      {title: "Build Node Api"},
+      {web: "Medium.com" }
+    ]
+  });
+});
 app.listen(PORT, ()=>console.log(`Task API - Port ${PORT}`));
