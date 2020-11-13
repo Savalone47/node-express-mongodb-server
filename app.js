e67d85ca493b8b67bfcd3b090379c49330d6f38e
savalone47 <savalonescott47@gmail.com>
2020-11-13 16:31:55 +0200

    catch 404 error

--- app.js
+++ app.js
@@ -10,3 +10,16 @@ consign()
     .then("libs/boot.js")
     .then("routes")
     .into(app);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+	let err = new Error('File Not Found');
+	err.status = 404;
+	next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+	res.status(err.status || 500);
+	res.send(err.message);
+});
--- in.js
+++ in.js
@@ -1,5 +1,8 @@
 module.exports = app => {
+  
   app.listen(app.get("port"), () => {
-    console.log(`Task API - Port ${app.get("port")}`);
+    
+    console.log(`API - Port ${app.get("port")}`);
+  
   });
 };
